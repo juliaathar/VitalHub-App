370fef3a1f121a4ce3d0818e31d7bf6c51f7e36e
juliaathar <juathar@gmail.com>
2024-02-05 10:53:58 -0300

    :heavy_plus_sign: Adicionado dependencias
    Adicionado dependencias de Styled-Components, Axios, React Navigation e Google Fonts

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//instancia do StackNavigator
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //Navegacao 
+      //Container
+      //StackNavigator
+      //StackScreen
+
+      // envolve a estrutura da navegacao
+      <NavigationContainer>
+
+        <Stack.Navigator>
+          
+        </Stack.Navigator>
+
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
