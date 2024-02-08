895cd6afd41df7cc0b3605d831a3c2309062d458
juliaathar <juathar@gmail.com>
2024-02-08 08:41:23 -0300

    :sparkles: Tela de redefinir senha feita

--- src/screens/setPassword/setPassword.js
+++ src/screens/setPassword/setPassword.js
@@ -1,7 +1,31 @@
 import { Text } from "react-native";
+import { Container } from "../../components/Container/Style";
+import { Logo } from "../../components/Logo/Style";
+import { Title } from "../../components/Title/Style";
+import { DefaultText } from "../../components/DefaultText/DefaultText";
+import { Input } from "../../components/Input/Style";
+import { Button, ButtonTitle } from "../../components/Buttons/Style";
 
 export const SetPassword = () => {
     return (
-        <Text>Tela de redefinir</Text>
+        <Container>
+            <Logo source={require('../../assets/logo.png')} />
+
+            <Title>Redefinir senha</Title>
+
+            <DefaultText>Insira e confirme a sua nova senha</DefaultText>
+
+            <Input
+                placeholder = "Nova senha"
+            />
+            <Input
+                placeholder = "Confirmar nova senha"
+            />
+
+            <Button>
+                <ButtonTitle>Confirmar nova senha</ButtonTitle>
+            </Button>
+            
+        </Container>
     );
 }
