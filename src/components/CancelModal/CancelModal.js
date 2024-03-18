5681bce5dea953ddbc919b05249fce20d8872112
juliaathar <juathar@gmail.com>
2024-03-18 10:09:46 -0300

    :sparkles: Notification implementada

--- src/components/CancelModal/CancelModal.js
+++ src/components/CancelModal/CancelModal.js
@@ -4,9 +4,48 @@ import { Title } from "../Title/Style";
 import { Button, ButtonModal, ButtonSecondary, ButtonTitle, ButtonTitleSecondary } from "../Buttons/Style";
 import { ModalContent, ModalText, PatientModal } from "./Style";
 
+//importar recursos da biblioteca
+import * as Notifications from "expo-notifications"
+
+//solicitar permissao de notificacao ao iniciar o app 
+Notifications.requestPermissionsAsync()
+
+//definir como as notificacoes devem ser tratadas quando recebidas 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    //mostra o alerta quando a notificacao for recebida
+    shouldShowAlert: true,
+
+    //ativa o som da notificacao
+    shouldPlaySound:true,
+
+    //configura exibicao de numero de notificacoes no icone do app
+    shouldSetBadge:true
+  })
+})
+
+
 const CancelModal = ({
     visible, setShowModalCancel, ...rest
 }) => {
+
+    const handleCallNotifications = async () => {
+        const {status} = await Notifications.getPermissionsAsync()
+
+        if(status !== "granted"){
+          alert("voce nao deixou as notificacoes ativas")
+          return;
+        }
+        
+        await Notifications.scheduleNotificationAsync({
+          content: {
+            title: "Consulta cancelada",
+            body:"Sua consulta foi cancelada"
+          },
+          trigger: null
+        });
+      }
+
     return (
         <Modal {...rest} visible={visible} transparent={true} animationType="fade">
             <PatientModal>
@@ -15,7 +54,7 @@ const CancelModal = ({
                     <ModalText>
                         Ao cancelar essa consulta, abrirá uma possível disponibilidade no seu horário, deseja mesmo cancelar essa consulta?
                     </ModalText>
-                    <ButtonModal>
+                    <ButtonModal onPress={handleCallNotifications}>
                         <ButtonTitle>Confirmar</ButtonTitle>
                     </ButtonModal>
                     <ButtonSecondary onPress={() => setShowModalCancel(false)}>
